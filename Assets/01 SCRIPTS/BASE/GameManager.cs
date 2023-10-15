a325f3317d66d3ab08f3e8d4e8d88e0d407f70e1
TanDao58 <daonhattan0508@gmail.com>
2023-10-15 15:30:06 +0700

    -test-commit

--- Assets/01 SCRIPTS/BASE/GameManager.cs
+++ Assets/01 SCRIPTS/BASE/GameManager.cs
@@ -6,6 +6,9 @@ public class CheckArray
 {
     public int[] _count_pawn_in_pos = new int[100];
     public int[] _start_pos_type_pawn = new int[100];
+
+    public IPawnable[] _list_pawn_switching = new IPawnable[100];
+    public Transform[] _list_pos_switching = new Transform[100];
 }
 
 public class GameManager : MonoBehaviour
@@ -15,11 +18,13 @@ public class GameManager : MonoBehaviour
     private IPawnable[] _list_Pos_inWinPos;
     int _countPawninWinPos = 0;
 
-    IPawnable pawnable, pawnable_switching;
+    IPawnable pawnable;
+
     private bool NotMouseClick;
     private bool NotRotate;
-    Transform _win_pos_move, _switching_pos_move;
+    Transform _win_pos_move;
 
+    private bool IsLoseGame = false;
     private void Start()
     {
         //Setup android
@@ -29,41 +34,48 @@ public class GameManager : MonoBehaviour
 
         //Setup check array
         _list_Pos_inWinPos = new IPawnable[_finish_parent_trans.childCount];
-
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (!IsLoseGame)
         {
-            CheckClickPawn(Input.mousePosition);
-        }
-        if (pawnable_switching != null && pawnable_switching.IsSwitchingPos)
-        {
-            if (Vector3.Distance(pawnable_switching.transform.position, _switching_pos_move.position) > 0.01f)
-                pawnable_switching.MovePawn(_switching_pos_move.position);
-            else
-                pawnable_switching.IsSwitchingPos = false;
-        }
-        if (NotMouseClick)
-        {
-            if (pawnable != null && !pawnable.IsInWinPos)
+            if (Input.GetMouseButtonDown(0))
             {
-                if (Vector3.Distance(pawnable.transform.position, _win_pos_move.position) > 0.01f)
+                CheckClickPawn(Input.mousePosition);
+            }
+            for (int i = 0; i < _check_array._list_pawn_switching.Length; i++)
+            {
+                if (_check_array._list_pawn_switching[i] != null && _check_array._list_pawn_switching[i].IsSwitchingPos)
                 {
-                    MovePawn(pawnable, _win_pos_move.position);
-                }
-                else
-                {
-                    ScalePawn(pawnable, 1.25f);
-                    pawnable.IsInWinPos = true;
-                    NotMouseClick = false;
+                    if (Vector3.Distance(_check_array._list_pawn_switching[i].transform.position, _check_array._list_pos_switching[i].position) > 0.01f)
+                        _check_array._list_pawn_switching[i].MovePawn(_check_array._list_pos_switching[i].position);
+                    else
+                    {
+                        _check_array._list_pawn_switching[i].IsSwitchingPos = false;
+                        _check_array._list_pawn_switching[i] = null;
+                        _check_array._list_pos_switching[i] = null;
+                    }
                 }
             }
+            if (NotMouseClick)
+            {
+                if (pawnable != null && !pawnable.IsInWinPos)
+                {
+                    if (Vector3.Distance(pawnable.transform.position, _win_pos_move.position) > 0.01f)
+                    {
+                        MovePawn(pawnable, _win_pos_move.position);
+                    }
+                    else
+                    {
+                        ScalePawn(pawnable, 1.5f);
+                        pawnable.IsInWinPos = true;
+                        NotMouseClick = false;
+                    }
+                }
 
+            }
         }
-
-
     }
 
     public void CheckClickPawn(Vector3 _checkPos)
@@ -71,9 +83,14 @@ public class GameManager : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(_checkPos);
         if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, LayerMask.GetMask("Pawn")))
         {
-            pawnable = raycastHit.collider.GetComponent<IPawnable>();
-            NumAddPawnToWinPos(pawnable);
-
+            pawnable = raycastHit.collider.transform.GetComponentInChildren<IPawnable>();
+            if (_list_Pos_inWinPos[_list_Pos_inWinPos.Length - 1] == null)
+                NumAddPawnToWinPos(pawnable);
+            else
+            {
+                IsLoseGame = true;
+                return;
+            }
 
             NotRotate = true;
             NotMouseClick = true;
@@ -102,18 +119,10 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-            int _sum_count_before_pawn = 0;
-            for (int i = 0; i < _check_array._count_pawn_in_pos.Length; i++)
-            {
-                if (_check_array._start_pos_type_pawn[i] < _check_array._start_pos_type_pawn[pawnable.PawnID])
-                    _sum_count_before_pawn += _check_array._count_pawn_in_pos[i];
-            }
-            Debug.Log(_sum_count_before_pawn);
-            for (int i = _list_Pos_inWinPos.Length - 1; i > _check_array._start_pos_type_pawn[pawnable.PawnID] + _check_array._count_pawn_in_pos[pawnable.PawnID] + _sum_count_before_pawn; i--)
+            for (int i = _list_Pos_inWinPos.Length - 1; i > _check_array._start_pos_type_pawn[pawnable.PawnID] + _check_array._count_pawn_in_pos[pawnable.PawnID]; i--)
             {
                 if (_list_Pos_inWinPos[i] != null)
                 {
-                    //Thua
                     return;
                 }
                 else
@@ -121,9 +130,11 @@ public class GameManager : MonoBehaviour
                     if (_list_Pos_inWinPos[i - 1] != null)
                     {
                         _list_Pos_inWinPos[i] = _list_Pos_inWinPos[i - 1];
-                        pawnable_switching = _list_Pos_inWinPos[i - 1];
-                        _switching_pos_move = _finish_parent_trans.GetChild(i);
-                        pawnable_switching.IsSwitchingPos = true;
+
+                        _check_array._list_pawn_switching[i - 1] = _list_Pos_inWinPos[i - 1];
+                        _check_array._list_pos_switching[i - 1] = _finish_parent_trans.GetChild(i);
+                        _list_Pos_inWinPos[i - 1].IsSwitchingPos = true;
+                        _list_Pos_inWinPos[i - 1] = null;
                     }
                 }
             }
@@ -131,12 +142,13 @@ public class GameManager : MonoBehaviour
             for (int i = 0; i < _check_array._start_pos_type_pawn.Length; i++)
             {
                 if (_check_array._start_pos_type_pawn[i] != 0 && _check_array._start_pos_type_pawn[i] > _check_array._start_pos_type_pawn[pawnable.PawnID])
+                {
                     _check_array._start_pos_type_pawn[i]++;
+                }
             }
         }
         _win_pos_move = _finish_parent_trans.GetChild(_check_array._start_pos_type_pawn[pawnable.PawnID] + _check_array._count_pawn_in_pos[pawnable.PawnID]);
         _check_array._count_pawn_in_pos[pawnable.PawnID]++;
         _countPawninWinPos++;
     }
-
 }
